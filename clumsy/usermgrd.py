cd52689aab97f0d90eceba7eccb43ad810de32be
Lars-Dominik Braun <ldb@leibniz-psychology.org>
2020-01-24 11:00:23 +0100

    usermgrd: Add rollback support
    Fixes #3.

--- clumsy/usermgrd.py
+++ clumsy/usermgrd.py
@@ -4,17 +4,21 @@ Create and delete user accounts on behalf of other daemons.
 Users must be local to the system this service is running on.
 """
 
-import secrets, bonsai, random
+import secrets, bonsai, random, functools
+from contextlib import AsyncExitStack
 
 import aiohttp
-from sanic import Sanic, Blueprint, response
+from sanic import Blueprint, response
 from sanic.log import logger
+from sanic.exceptions import ServerError, Forbidden, NotFound, SanicException
 
 from .nss import getUser
 from .kadm import KAdm, KAdmException
 
 ldapc = None
 kadm = None
+flushsession = None
+homedirsession = None
 reservedUid = set ()
 
 def randomSecret (n):
@@ -25,32 +29,62 @@ def socketSession (path):
 	conn = aiohttp.UnixConnector (path=path)
 	return aiohttp.ClientSession(connector=conn)
 
-async def flushUserCache (sockpath):
+async def flushUserCache ():
 	"""
 	Flush the local user caches
 	"""
-	async with socketSession (sockpath) as session:
-		# hostname does not matter for unix domain socket?
-		async with session.delete (f'http://localhost/account') as resp:
-			deldata = await resp.json ()
-			if deldata['status'] != 'ok':
-				return response.json ({'status': 'cache_flush'}, status=500)
+	# hostname does not matter for unix domain socket?
+	async with flushsession.delete (f'http://localhost/account') as resp:
+		deldata = await resp.json ()
+		if deldata['status'] != 'ok':
+			raise ServerError ({'status': 'flush_failed', 'nscdflush_status': deldata['status']})
 
 bp = Blueprint('usermgrd')
 
 @bp.listener('before_server_start')
 async def setup (app, loop):
-	global ldapc, kadm
+	global ldapc, kadm, flushsession, homedirsession
 
 	config = app.config
 
-	ldapc = bonsai.LDAPClient (config.LDAP_SERVER)
-	ldapc.set_credentials ("SIMPLE", user=config.LDAP_USER, password=config.LDAP_PASSWORD)
+	client = bonsai.LDAPClient (config.LDAP_SERVER)
+	client.set_credentials ("SIMPLE", user=config.LDAP_USER, password=config.LDAP_PASSWORD)
+	ldapc = await client.connect (is_async=True)
 
 	kadm = KAdm (config.KERBEROS_USER, config.KERBEROS_KEYTAB)
 
+	flushsession = socketSession (config.NSCDFLUSHD_SOCKET)
+	homedirsession = socketSession (config.MKHOMEDIRD_SOCKET)
+
+@bp.listener('after_server_stop')
+async def teardown (app, loop):
+	flushsession.close ()
+	homedirsession.close ()
+	ldapc.close ()
+
+@bp.exception(SanicException)
+async def handleErrors (request, exc):
+	assert 'status' in exc.message
+	return response.json (exc.message, status=exc.status_code)
+
+def withRollback (func):
+	"""
+	Rollback operations performed by func if it fails (i.e. Exception is raised)
+
+	func is passed an additional argument rollback and is responsible for
+	adding callbacks to it using push_async_callback() or callback().
+	"""
+	@functools.wraps(func)
+	async def wrapped(*args, **kwargs):
+		async with AsyncExitStack () as stack:
+			ret = await func(*args, **kwargs, rollback=stack)
+			stack.pop_all ()
+			return ret
+	return wrapped
+
 @bp.route ('/', methods=['POST'])
-async def addUser (request):
+@withRollback
+async def addUser (request, rollback):
 	config = request.app.config
 
 	while True:
@@ -68,48 +102,48 @@ async def addUser (request):
 				reservedUid.add (uid)
 				break
 
-	async with ldapc.connect (is_async=True) as conn:
-		o = bonsai.LDAPEntry (f"uid={user},ou=people,dc=compute,dc=zpid,dc=de")
-		o['objectClass'] = [
-				'top',
-				'person',
-				'organizationalPerson',
-				'inetOrgPerson',
-				'posixAccount',
-				'shadowAccount',
-				]
-		o['sn'] = f'Project account {user}'
-		o['cn'] = f'{user}'
-		o['loginShell'] = '/bin/bash'
-		o['uidNumber'] = uid
-		o['gidNumber'] = gid
-		o['uid'] = user
-		o['homeDirectory'] = f'/home/{user}'
-		await conn.add (o)
+	o = bonsai.LDAPEntry (f"uid={user},ou=people,dc=compute,dc=zpid,dc=de")
+	o['objectClass'] = [
+			'top',
+			'person',
+			'organizationalPerson',
+			'inetOrgPerson',
+			'posixAccount',
+			'shadowAccount',
+			]
+	o['sn'] = f'Project account {user}'
+	o['cn'] = f'{user}'
+	o['loginShell'] = '/bin/bash'
+	o['uidNumber'] = uid
+	o['gidNumber'] = gid
+	o['uid'] = user
+	o['homeDirectory'] = f'/home/{user}'
+	await ldapc.add (o)
+	rollback.push_async_callback (o.delete)
 
-		o = bonsai.LDAPEntry (f"cn={user},ou=group,dc=compute,dc=zpid,dc=de")
-		o['objectClass'] = ['top', 'posixGroup']
-		o['cn'] = user
-		o['gidNumber'] = gid
-		o['memberUid'] = user
-		await conn.add (o)
-	await flushUserCache (config.NSCDFLUSHD_SOCKET)
+	o = bonsai.LDAPEntry (f"cn={user},ou=group,dc=compute,dc=zpid,dc=de")
+	o['objectClass'] = ['top', 'posixGroup']
+	o['cn'] = user
+	o['gidNumber'] = gid
+	o['memberUid'] = user
+	await ldapc.add (o)
+	rollback.push_async_callback (o.delete)
+
+	await flushUserCache ()
 	reservedUid.remove (uid)
 
 	try:
 		logger.debug ('adding kerberos user')
 		await kadm.addPrincipal (user, password)
+		rollback.push_async_callback (kadm.deletePrincipal, user)
 	except KAdmException:
-		return response.json ({'status': 'kerberos_failed', 'ldap_code': ret}, status=500)
+		raise ServerError ({'status': 'kerberos_failed'})
 
 	# create homedir
-	async with socketSession (config.MKHOMEDIRD_SOCKET) as session:
-		async with session.post (f'http://localhost/{user}') as resp:
-			data = await resp.json ()
-			if data['status'] != 'ok':
-				return response.json ({'status': 'mkhomedir_failed', 'mkhomedird_status': data['status']})
-
-	# XXX: roll back, in case anything above fails
+	async with homedirsession.post (f'http://localhost/{user}') as resp:
+		data = await resp.json ()
+		if data['status'] != 'ok':
+			raise ServerError ({'status': 'mkhomedir_failed', 'mkhomedird_status': data['status']})
 
 	return response.json ({'status': 'ok', 'user': user, 'password': password, 'uid': uid, 'gid': uid}, status=201)
 
@@ -127,10 +161,10 @@ async def deleteUser (request, user):
 	try:
 		res = getUser (user)
 	except KeyError:
-		return response.json ({'status': 'user_not_found'}, status=404)
+		raise NotFound ({'status': 'user_not_found'})
 
 	if not (config.MIN_UID <= res['uid'] < config.MAX_UID):
-		return response.json ({'status': 'unauthorized'}, status=403)
+		raise Forbidden ({'status': 'unauthorized'})
 
 	# disallow logging in by deleting principal
 	try:
@@ -138,30 +172,26 @@ async def deleteUser (request, user):
 		# XXX: race-condition
 		await kadm.deletePrincipal (user)
 	except KeyError:
-		logger.warn (f'kerberos principal for {user} already gone')
+		logger.warning (f'kerberos principal for {user} already gone')
 	except KAdmException:
-		return response.json ({'status': 'kerberos_failed', 'ldap_code': ret}, status=500)
+		raise ServerError ({'status': 'kerberos_failed'})
 
 	# mark homedir for deletion
-	session = socketSession (config.MKHOMEDIRD_SOCKET)
-	async with session.delete (f'http://localhost/{user}') as resp:
+	async with homedirsession.delete (f'http://localhost/{user}') as resp:
 		deldata = await resp.json ()
 		if deldata['status'] != 'again':
-			return response.json ({'status': 'mkhomedird_token', 'mkhomedird_status': deldata['status']})
+			raise ServerError ({'status': 'mkhomedird_token', 'mkhomedird_status': deldata['status']})
 
-	async with ldapc.connect (is_async=True) as conn:
-		await conn.delete (f"uid={user},ou=people,dc=compute,dc=zpid,dc=de")
-		await conn.delete (f"cn={user},ou=group,dc=compute,dc=zpid,dc=de")
+	await ldapc.delete (f"uid={user},ou=people,dc=compute,dc=zpid,dc=de")
+	await ldapc.delete (f"cn={user},ou=group,dc=compute,dc=zpid,dc=de")
 
-	await flushUserCache (config.NSCDFLUSHD_SOCKET)
+	await flushUserCache ()
 
 	# finally delete homedir
-	async with session.delete (f'http://localhost/{user}', params={'token': deldata['token']}) as resp:
+	async with homedirsession.delete (f'http://localhost/{user}', params={'token': deldata['token']}) as resp:
 		deldata = await resp.json ()
 		if deldata['status'] != 'ok':
-			return response.json ({'status': 'mkhomedir_delete', 'mkhomedird_status': deldata['status']})
-
-	await session.close ()
+			raise ServerError ({'status': 'mkhomedir_delete', 'mkhomedird_status': deldata['status']})
 
 	return response.json ({'status': 'ok'})
 
